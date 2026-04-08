09ccaa214ecef6b792b8c1d39926d7e0f519a89c
riddhichavan14 <riddhichavan41@gmail.com>
2026-04-08 19:15:00 +0530

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,13 +1,2 @@
-def grade(state):
-    score = 0.0
-
-    if state["latency"] < 100:
-        score += 0.3
-
-    if state["packet_loss"] < 0.05:
-        score += 0.3
-
-    if state["bandwidth"] > 75:
-        score += 0.4
-
-    return min(score, 1.0)
+def grade(output: str, expected: str) -> float:
+    return 0.5
